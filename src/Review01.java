3fb359dc7c3e0eb65d558f4daf3486c7885bba6e
yom2-aber <yoshihiromayama@gmail.com>
2023-11-14 07:06:51 +0900

    Lesson3課題提出

---
+++ src/Review01.java
@@ -0,0 +1,14 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        int num1 = 1500;
+        int num2 = tax(num1);
+        System.out.println(num1 + "円の商品の税込価格は" + (num1 + num2) +"円（消費税は" + num2 +"円）です。");
+    }
+
+    public static int tax(int num1) {
+        int result = num1 / 10;
+        return result;
+    }
+}
